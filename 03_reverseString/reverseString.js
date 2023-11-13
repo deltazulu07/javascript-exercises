d1694c34ffd1f87719a80fb28d80ef9ced37e79a
deltazulu07 <133428143+deltazulu07@users.noreply.github.com>
2023-11-13 11:32:01 -0800

    solutions for exercises 1 to 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversed = '';
+    for (let i = 0; i<input.length; i++)  {
+        reversed = input[i] + reversed;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, argument) {
+    let indexOfarg = 0;
+    while (indexOfarg !== -1) {
+        indexOfarg = list.indexOf(argument);
+        list.splice(indexOfarg, 1);
+    }
+    return list;
 
 };
 
