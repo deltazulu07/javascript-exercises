dc419be11050cf1943fe4de56bc097fb2a22a50f
JCO <133428143+deltazulu07@users.noreply.github.com>
2024-10-08 16:16:08 -0700

    updated

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,21 +1,16 @@
 const findTheOldest = function(arrOfPeople) {
     //Given an array of objects representing people with a birth and death year, return the oldest person object
 
-    const oldest = arrOfPeople.reduce((oldestPerson, currentPerson) => {
+    return arrOfPeople.reduce((oldestPerson, currentPerson) => {
 
-        function CalcAge(person) {
+        const CalcAge = function (person) {
             return ('yearOfDeath' in person) ? //ternary operator
                     person.yearOfDeath - person.yearOfBirth :
                     new Date().getFullYear() - person.yearOfBirth;
         }
 
-        if (CalcAge(currentPerson) > CalcAge(oldestPerson)) {
-            oldestPerson = currentPerson;
-        }
-        return oldestPerson;
-    }, arrOfPeople[0]);
-
-    return oldest;
+        return CalcAge(currentPerson) > CalcAge(oldestPerson) ? currentPerson : oldestPerson;
+    });
 }
 
 // Do not edit below this line
